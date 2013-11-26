eb05c027d1cb8cbd1dc6a1519fee861135c87434
TimoKoukkari <timo.koukkari@gmail.com>
2013-11-26 15:08:43 +0200

    First version

---
+++ Sensotrend/Glucose.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Sensotrend
+{
+    public partial class Glucose : PhoneApplicationPage
+    {
+        public Glucose()
+        {
+            InitializeComponent();   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double glucoseValue = 0;
+            try {
+                glucoseValue = Double.Parse(glucoseText.Text);
+            } catch (System.ArgumentNullException ex) {
+                MessageBox.Show("Cannot parse " + glucoseText.Text);
+            } catch (System.FormatException ex) {
+                MessageBox.Show("Cannot parse " + glucoseText.Text);
+
+            } catch (System.OverflowException ex) {
+                MessageBox.Show("Cannot parse " + glucoseText.Text);
+            }
+
+            // Send glucose value to Taltioni
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+
+        private void glucoseText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (glucoseText.Text == "")
+                okButton.IsEnabled = false;
+            else
+                okButton.IsEnabled = true;
+        }
+    }
+}
---
+++ Sensotrend/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+
+namespace Sensotrend
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Open connection to Taltioni
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Glucose.xaml", UriKind.Relative));
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            // Close connection to Taltioni
+        }
+    }
+}
